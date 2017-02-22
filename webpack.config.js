86ea2311e022f22ed0d07c1227aa1b5d85c22fb1
Vladimir Rodkin <mail@vovanr.com>
2017-02-22 18:47:20 +0300

    Update dev deps; webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,13 @@
+const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
+
+const ENV = {
+	production: 'production',
+	development: 'development'
+};
+
+const isDev = process.env.NODE_ENV !== ENV.production;
 
 module.exports = {
     entry: {
@@ -10,28 +18,65 @@ module.exports = {
         filename: '[name].js'
     },
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.css$/,
-                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!postcss-loader')
-            }
-        ]
-    },
-    plugins: [
-        new StyleLintPlugin({
-            files: './src/**/*.css'
-        }),
-        new ExtractTextPlugin('[name].css', {allChunks: true}),
-    ],
-    postcss: function (webpack) {
-        return [
-            require('postcss-import')({addDependencyTo: webpack}),
-            require('precss')(),
-            require('postcss-flexbugs-fixes')(),
-            require('postcss-clearfix')(),
-            require('postcss-style-guide')({dest: './dist/styleguide/index.html'}),
-            require('autoprefixer')({browsers: ['last 2 versions'], cascade: false}),
-            require('postcss-csso')()
-        ];
-    }
+                use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: [
+						{
+							loader: 'css-loader',
+							options: {
+								importLoaders: 1,
+								minimize: false,
+								url: false,
+								sourceMap: true
+							}
+						},
+						{
+							loader: 'postcss-loader',
+							options: {
+								// sourceMap: 'inline',
+								plugins: function () {
+									const plugins = [
+										require('precss')(),
+										require('postcss-flexbugs-fixes')(),
+										require('postcss-clearfix')(),
+                                        require('postcss-style-guide')({dest: './dist/styleguide/index.html'}),
+										require('autoprefixer')({cascade: false})
+									];
+
+									if (isDev) {
+										return plugins;
+									}
+
+									plugins.push(
+										require('postcss-csso')()
+									);
+									return plugins;
+								}
+							}
+						}
+					]
+				})
+			}
+		]
+	},
+	plugins: (() => {
+		const plugins = [
+			new webpack.DefinePlugin({
+				'process.env.NODE_ENV': JSON.stringify(isDev ? ENV.development : ENV.production)
+			}),
+		    new StyleLintPlugin({
+		        files: './src/**/*.css'
+		    }),
+			new ExtractTextPlugin({
+				filename: '[name].css',
+				allChunks: true
+			})
+		];
+
+		return plugins;
+	})(),
+	devtool: isDev && 'cheap-module-source-map'
 };
